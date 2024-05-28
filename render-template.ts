3708c08ec60891adafc964700fc35da2843da4b3
D. Bohdan <dbohdan@dbohdan.com>
2024-05-28 15:27:03 +0000

    render-template: add `--allow-env` in `#!` line

--- render-template.ts
+++ render-template.ts
@@ -1,4 +1,4 @@
-#! /usr/bin/env -S deno run --allow-read --check
+#! /usr/bin/env -S deno run --allow-read --allow-env --check
 
 import markdownTOC from "npm:markdown-toc@1";
 import nunjucks from "npm:nunjucks@3";
